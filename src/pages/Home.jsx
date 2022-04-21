58dd008b42a69b93ee99baeaf4ebc39bea2b4f8d
ipondnakab <kittipat_dd@kkumail.com>
2022-04-22 00:20:05 +0700

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import {
+  ActivityTimeline,
+  TimelineMarker,
+  Card,
+} from "react-rainbow-components";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="rainbow-m-around_xx-large">
+      <ActivityTimeline>
+        <TimelineMarker label="User first post." datetime="3 hours ago">
+          <Card title="Inside Content" style={{ padding: "16px" }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Card>
+        </TimelineMarker>
+        <TimelineMarker label="User first post." datetime="3 hours ago">
+          <Card title="Inside Content" style={{ padding: "16px" }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Card>
+        </TimelineMarker>
+        <TimelineMarker label="User first post." datetime="3 hours ago">
+          <Card title="Inside Content" style={{ padding: "16px" }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Card>
+        </TimelineMarker>
+      </ActivityTimeline>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
