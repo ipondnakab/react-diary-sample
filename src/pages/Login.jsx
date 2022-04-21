26a87bafbb34b4ff19c288214b612c811f91dc96
ipondnakab <kittipat_dd@kkumail.com>
2022-04-21 23:40:31 +0700

    Login, Register Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
+import React from "react";
+import { Input, Card, Button } from "react-rainbow-components";
 function LoginPage() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const onSubmit = () => {
+    console.log({ email, password });
+  };
+
   return (
-    <div>LoginPage</div>
-  )
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        style={{
+          width: "500px",
+          height: "500px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Login</h1>
+        <Input
+          label="Email"
+          labelAlignment="left"
+          placeholder="Placeholder text"
+          style={{ width: "300px" }}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          label="Password"
+          labelAlignment="left"
+          placeholder="Password"
+          type="password"
+          style={{ width: "300px" }}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          style={{ marginTop: "32px", width: "300px" }}
+          variant="brand"
+          onClick={onSubmit}
+          label="Login"
+        />
+      </Card>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Input, Card, Button } from "react-rainbow-components";
 
 function RegisterPage() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+
+  const onSubmit = () => {
+    console.log({ email, password, confirmPassword });
+  };
+
   return (
-    <div>RegisterPage</div>
-  )
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        style={{
+          width: "500px",
+          height: "500px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Register</h1>
+        <Input
+          label="Email"
+          labelAlignment="left"
+          placeholder="Placeholder text"
+          style={{ width: "300px" }}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          label="Password"
+          labelAlignment="left"
+          placeholder="Password"
+          type="password"
+          style={{ width: "300px" }}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Input
+          label="Confirm Password"
+          labelAlignment="left"
+          placeholder="Confirm Password"
+          type="password"
+          style={{ width: "300px" }}
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <Button
+          style={{ marginTop: "32px", width: "300px" }}
+          variant="brand"
+          onClick={onSubmit}
+          label="Register"
+        />
+      </Card>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
